e79bde15f6cd09a2d440e1caeed310e095e82321
Chathumi Rambukwella <61647996+Chathumiii@users.noreply.github.com>
2023-03-13 17:41:27 -0400

    Update securitygroup.tf

--- terraform/securitygroup.tf
+++ terraform/securitygroup.tf
@@ -3,28 +3,38 @@ resource "aws_security_group" "docker_sg" {
   description = "Security group for Docker Compose app"
   vpc_id      = data.aws_vpc.default.id
 
-  ingress {
-    from_port   = 5000
-    to_port     = 5000
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]
+ ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
   }
-
   ingress {
-    from_port   = 3306
-    to_port     = 3306
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]
+    from_port        = 5001
+    to_port          = 5001
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
   }
-
+  ingress {
+    from_port        = 5002
+    to_port          = 5002
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port        = 5003
+    to_port          = 5003
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
-  tags = {
+tags = {
     Terraform = "true"
     Environment = "dev"
   }
